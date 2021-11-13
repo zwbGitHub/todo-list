9768463c441399e41e21359e139d2af97826b8d0
张文博 <zwb960906@163.com>
2021-11-13 23:04:11 +0800

    打包压缩完成

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,24 @@
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+let isProduction = process.env.NODE_ENV
 module.exports = {
-  configureWebpack: (config) => {
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(
-        ...[
-          new CompressionWebpackPlugin({
-            filename: '[path].gz[query]',
-            algorithm: 'gzip',
-            test: /\.(js|css|html|svg)$/i,
-            threshold: 2048,
-            minRatio: 0.8,
-          }),
-        ]
-      )
-    }
-  },
+    configureWebpack: config => {
+        // 生产环境相关配置
+        if (isProduction == "production") {
+        // 代码压缩
+        config.plugins.push(
+        new UglifyJsPlugin({
+        uglifyOptions: {
+        //生产环境自动删除 console
+        warnings: false,
+        compress: {
+        // warnings: false, // 若打包错误，则注释这行
+        drop_debugger: true,
+        drop_console: true,
+        pure_funcs: ['console.log']
+        }
+        },
+        sourceMap: false,
+        parallel: true
+        })
+        ) } }
 }
